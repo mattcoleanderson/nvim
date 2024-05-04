a8502e1f8975dfdb8f88c8e7f5f060269abe0044
Matt Cole Anderson <github@mattcoleanderson.com>
2024-05-03 22:16:45 -0500

    Add remaps for Windows, saving, and telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,3 +1,3 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>sf', builtin.find_files, {}) -- Search all project files
-vim.keymap.set('n', '<leader>sg', builtin.git_files, {})  -- Search git files
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {}) -- Search all project files
+vim.keymap.set('n', '<leader>fg', builtin.git_files, {})  -- Search git files
--- lua/manderson/remap.lua
+++ lua/manderson/remap.lua
@@ -1,2 +1,9 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
+
+-- Save
+vim.keymap.set("n", "<leader>s", ':w<CR>')
+
+-- Windows
+vim.keymap.set("n", "<leader>ws", '<C-w>s') -- Windows now accessed with leader-w
+
