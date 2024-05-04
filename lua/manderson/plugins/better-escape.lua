09c585ebca07c9479cb1596e04a02c1906355c97
Matt Cole Anderson <github@mattcoleanderson.com>
2024-05-03 23:53:28 -0500

    update better-escape.lua
    require(MAIN).setup(opt) is the default logic of config. So I removed
    the custome config

--- lua/manderson/plugins/better-escape.lua
+++ lua/manderson/plugins/better-escape.lua
@@ -3,8 +3,5 @@ return {
     'max397574/better-escape.nvim',
     main = 'better_escape',
     config = true
---     config = function()
---      require('better_escape').setup()
---    end, 
   }
 }
