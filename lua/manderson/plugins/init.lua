e9d255a261c93b5c6ae42e090286bba941d52019
Matt Cole Anderson <github@mattcoleanderson.com>
2024-05-03 23:09:44 -0500

    extract telescope plugin

--- lua/manderson/plugins/init.lua
+++ lua/manderson/plugins/init.lua
@@ -1,11 +1,6 @@
 -- Setup Plugins
 return {
   {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.6',
-    dependencies = {'nvim-lua/plenary.nvim'}
-  },
-  {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate"
   },
