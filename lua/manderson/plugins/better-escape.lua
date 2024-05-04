1cbdddc49d21a0df3f14f0f8a4bcc7263f9a7bea
Matt Cole Anderson <github@mattcoleanderson.com>
2024-05-03 23:48:20 -0500

    Add better-escape.lua

---
+++ lua/manderson/plugins/better-escape.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    'max397574/better-escape.nvim',
+    main = 'better_escape',
+    config = true
+--     config = function()
+--      require('better_escape').setup()
+--    end, 
+  }
+}
