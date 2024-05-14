31659d70fb5cf463bd44ef4a0238b28ddf33c272
Matt Cole Anderson <github@mattcoleanderson.com>
2024-05-14 12:28:26 -0500

    add comment

--- lua/manderson/lazy.lua
+++ lua/manderson/lazy.lua
@@ -12,5 +12,6 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+-- modularize lazy by autoloading any file under ./plugins directory
 require('lazy').setup('manderson/plugins')
 
