039e6ddf7f6716c2cfdfafdd3cbe9c390f6cfff2
Matt Cole Anderson <github@mattcoleanderson.com>
2024-05-14 12:46:06 -0500

    lazy load telescope on keypress

--- after/plugin/telescope.lua
+++
@@ -1,3 +0,0 @@
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {}) -- Search all project files
-vim.keymap.set('n', '<leader>fg', builtin.git_files, {})  -- Search git files
--- lua/manderson/plugins/telescope.lua
+++ lua/manderson/plugins/telescope.lua
@@ -2,6 +2,10 @@ return {
   {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.6',
-    dependencies = {'nvim-lua/plenary.nvim'}
+    dependencies = {'nvim-lua/plenary.nvim'},
+    keys = {
+      { '<leader>ff', '<cmd>Telescope find_files<cr>', desc = 'Telescope: Search all project files' },
+      { '<leader>fg', '<cmd>Telescope git_files<cr>', desc = 'Telescope: Search git files' }
+    }
   }
 }
