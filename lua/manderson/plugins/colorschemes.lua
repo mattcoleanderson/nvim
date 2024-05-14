b7004be16a360e871b3567aadead73cb299b3f5d
Matt Cole Anderson <github@mattcoleanderson.com>
2024-05-14 14:05:04 -0500

    migrate colors.lau to colorschemes.lua
    utilizing the config property when setting up catppuccin through lazy

--- lua/manderson/plugins/colorschemes.lua
+++ lua/manderson/plugins/colorschemes.lua
@@ -4,6 +4,13 @@ return {
     'catppuccin/nvim',
     lazy = false,
     name = 'catppuccin',
-    priority = 1000
+    priority = 1000,
+    config = function()
+      -- Set colorscheme
+      vim.cmd.colorscheme('catppuccin')
+      -- Add some transparency
+      vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+      vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+    end
   }
 }
