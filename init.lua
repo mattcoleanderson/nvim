c2418f60b802e1ef362ddc2880aae4e1bb08389d
Matt Cole Anderson <github@mattcoleanderson.com>
2024-05-14 16:26:49 -0500

    remove 'manderson' directory
    it was an unecessary directory that added complexity when configuring
    lazy.nvim

--- init.lua
+++ init.lua
@@ -1,3 +1,17 @@
-require('manderson/options')
-require('manderson/remap')
-require('manderson/lazy')
+--plugins, opts) Bootstrap Lazy.nvim plugin manager
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require('options')
+require('remap')
+require('lazy').setup('plugins') -- top-level files in lua/plugins will be auto-required
