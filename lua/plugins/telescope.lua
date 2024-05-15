aa152fc6dcefbe5459042b68e4fdf801b94decd9
Matt Cole Anderson <github@mattcoleanderson.com>
2024-05-15 14:41:14 -0500

    add telescope-ui-select extension for telescope
    allows nvim core menus to be shown through a telescope picker

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,10 +2,29 @@ return {
   {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.6',
-    dependencies = {'nvim-lua/plenary.nvim'},
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-telescope/telescope-ui-select.nvim',
+    },
     keys = {
       { '<leader>ff', '<cmd>Telescope find_files<cr>', desc = 'Telescope: Search all project files' },
       { '<leader>fg', '<cmd>Telescope git_files<cr>', desc = 'Telescope: Search git files' }
-    }
+    },
+    opts = function()
+      local theme = require("telescope.themes").get_dropdown({})
+      return {
+        extensions = {
+          ["ui-select"] = {
+            theme
+          }
+        }
+      }
+    end,
+    config = function(_, opts)
+      local plugin = require('telescope')
+
+      plugin.setup(opts)
+      plugin.load_extension('ui-select')
+    end
   }
 }
