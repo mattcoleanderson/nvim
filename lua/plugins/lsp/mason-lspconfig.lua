97d7b785b4cf7140abccc34d122d776868ff34c2
Matt Cole Anderson <github@mattcoleanderson.com>
2024-05-16 15:23:19 -0500

    add tsserver

--- lua/plugins/lsp/mason-lspconfig.lua
+++ lua/plugins/lsp/mason-lspconfig.lua
@@ -4,6 +4,7 @@ return {
     opts = {
       ensure_installed = {
         'lua_ls@3.7.4',
+        'tsserver'
       }
     },
   }
--- lua/plugins/lsp/nvim-lspconfig.lua
+++ lua/plugins/lsp/nvim-lspconfig.lua
@@ -3,6 +3,8 @@ return {
     'neovim/nvim-lspconfig',
     config = function()
       local lspconfig = require('lspconfig')
+
+      lspconfig.tsserver.setup({})
       lspconfig.lua_ls.setup({
 --         settings = {
 --           Lua = {
