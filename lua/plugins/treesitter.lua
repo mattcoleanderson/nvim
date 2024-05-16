48f69c24f9e70e158a35b74eab8f923144916bb3
Matt Cole Anderson <github@mattcoleanderson.com>
2024-05-16 16:34:30 -0500

    auto install parsers with treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,12 +4,6 @@ return {
     build = ':TSUpdate',
     main = 'nvim-treesitter.configs',
     opts = {
-      -- A list of parser names, or 'all' (the five listed parsers should always be installed)
-      ensure_installed = { 'bash', 'javascript', 'typescript', 'python', 'go', 'c', 'lua', 'vim', 'vimdoc', 'query' },
-
-      -- Install parsers synchronously (only applied to `ensure_installed`)
-      sync_install = false,
-
       -- Automatically install missing parsers when entering buffer
       -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
       auto_install = true,
