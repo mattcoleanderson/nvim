8c355c37f761b0725121efff718c0f8f5b6ec428
Matthew Anderson <matthewanderson@FO-MAnderson-MBPM1PRO.local>
2024-07-11 11:06:17 -0500

    add obsidian plugin for working with obsidian.md notes

---
+++ lua/plugins/obsidian.lua
@@ -0,0 +1,25 @@
+local obsidian_dir = vim.fn.expand('~') .. '/repos/personal/obsidian-personal-vault'
+
+return {
+  'epwalsh/obsidian.nvim',
+  version = '*', -- '*' installs the latest stable version from github
+  lazy = true,
+  event = {
+    'BufReadPre ' .. obsidian_dir .. '/**.md',
+    'BufNewFile ' .. obsidian_dir .. '/**.md',
+  },
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+  },
+  keys = {
+    { '<leader>of', '<cmd>ObsidianFollowLink<cr>', desc = 'Obsidian: Follow link under cursor' },
+  },
+  opts = {
+    workspaces = {
+      {
+        name = 'personal',
+        path = obsidian_dir,
+      },
+    },
+  },
+}
