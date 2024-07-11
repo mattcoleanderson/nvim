dc07034d6a7dfcdb5e68bfe7b075ed7b51f21ba5
Matthew Anderson <matthewanderson@FO-MAnderson-MBPM1PRO.local>
2024-07-11 11:05:46 -0500

    convert options from vim.cmd to vim.opt

--- lua/options.lua
+++ lua/options.lua
@@ -1,4 +1,24 @@
-vim.cmd('set expandtab')
-vim.cmd('set tabstop=2')
-vim.cmd('set softtabstop=2')
-vim.cmd('set shiftwidth=2')
+--[[
+There are many ways to set vim options including:
+  vim.cmd('set wrap')
+  vim.opt.wrap = false
+  vim.o.wrap = false
+]]
+
+
+-- Set all vim options in this table
+local options = {
+  number = true,          -- Show absolute line numbers
+  relativenumber = true,  -- Show relative line numbers
+  expandtab = true,
+  tabstop = 4,
+  softtabstop = 2,
+  shiftwidth = 2,
+  wrap = false,
+  conceallevel = 2,
+}
+
+-- The above table will be iterated to set options individually
+for option, value in pairs(options) do
+  vim.opt[option] = value
+end
