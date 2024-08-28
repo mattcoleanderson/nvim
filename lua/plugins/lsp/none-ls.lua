1f000018a0b12fb78dc122763b2f785c033dd7fc
Matthew Anderson <matthewanderson@FO-MAnd-MBPM1P.local>
2024-08-28 13:59:02 -0500

    Add comments to none-ls.lua to describe what is is

--- lua/plugins/lsp/none-ls.lua
+++ lua/plugins/lsp/none-ls.lua
@@ -1,9 +1,14 @@
+-- none-ls is the predecessor to null-ls
+-- An API to expose Formatters, Linters, and Parsers to the LSP API
+-- These tools are typically command line.
+-- with none-ls they can be used with editor commands and events
 return {
   'nvimtools/none-ls.nvim',
   config = function()
-    local null_ls = require('null-ls')
+    local null_ls = require('null-ls') -- none-ls is still internally called null-ls
 
     null_ls.setup({
+      -- Add Formatters, Linters, and Parsers you'd wish to be exposed to LSP API
       sources = {
         null_ls.builtins.formatting.stylua,
       },
