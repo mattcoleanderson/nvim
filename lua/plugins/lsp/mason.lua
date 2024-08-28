a2bb107f7514da897cdf25ffb410c86eff85c434
Matthew Anderson <matthewanderson@FO-MAnd-MBPM1P.local>
2024-08-28 13:58:18 -0500

    Combine mason and mason-lspconfig into a single lua file

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -1,6 +1,17 @@
+-- Mason is a package manager for LSPs, Linters, Parsers, and Formatters
 return {
   {
-    'williamboman/mason.nvim',
+    'williamboman/mason.nvim', -- Mason Package manager
     config = true,
+  },
+  {
+    'williamboman/mason-lspconfig.nvim', -- Helper function to explicitly state LSPs to install.
+    opts = {
+      ensure_installed = {
+        'lua_ls@3.7.4', -- Lua
+        'tsserver',     -- TypeScript
+        'texlab',       -- LaTeX
+      },
+    },
   },
 }
