12e4e0dc35a123f74ddc0e1be1de2e4db78749cc
Matthew Anderson <matthewanderson@FO-MAnd-MBPM1P.local>
2024-08-28 14:03:18 -0500

    Applied formatting to keymap.lua

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -9,12 +9,12 @@ vim.keymap.set('n', '<leader>s', ':w<CR>', { desc = 'Save the current buffer in 
 -- Windows
 -- if you would like to add more window commands type `:h CTRL-W`
 wk.add({
-  { '<leader>w', proxy = '<c-w>',      group = 'window' }, -- proxy to window mappings
+  { '<leader>w', proxy = '<c-w>',                                group = 'window' }, -- proxy to window mappings
   { '<c-w>c',    desc = 'Close window' },
-  { '<c-w>H', desc = 'move current window to the far left' },
-  { '<c-w>J', desc = 'move current window to the very bottom' },
-  { '<c-w>K', desc = 'move current window to the very top' },
-  { '<c-w>L', desc = 'move current window to the far right' },
+  { '<c-w>H',    desc = 'move current window to the far left' },
+  { '<c-w>J',    desc = 'move current window to the very bottom' },
+  { '<c-w>K',    desc = 'move current window to the very top' },
+  { '<c-w>L',    desc = 'move current window to the far right' },
 })
 
 -- Custom Commands
