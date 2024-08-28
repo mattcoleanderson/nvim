15a69d56084b2da07781a1afa9427d48507d8864
Matthew Anderson <matthewanderson@FO-MAnd-MBPM1P.local>
2024-08-27 23:43:27 -0500

    add which-key groups for lsp and diagnostics

--- lua/plugins/lsp/nvim-lspconfig.lua
+++ lua/plugins/lsp/nvim-lspconfig.lua
@@ -3,15 +3,18 @@ return {
     'neovim/nvim-lspconfig',
     config = function()
       local lspconfig = require('lspconfig')
+      local wk = require('which-key')
 
       lspconfig.tsserver.setup({})
       lspconfig.lua_ls.setup({})
       lspconfig.texlab.setup({})
 
       -- Global mappings
-      vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, {})
-      vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-      vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+      -- Diagnostics
+      wk.add({ '<leader>d', group = 'diagnositcs' })
+      vim.keymap.set('n', '<leader>de', vim.diagnostic.open_float, { desc = 'Open diagnostic modal' })
+      vim.keymap.set('n', '<leader>dl', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic' })
+      vim.keymap.set('n', '<leader>dh', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic' })
 
       -- LspAttach autocommand will only map the following keys
       -- after the language server attaches to the current buffer
@@ -22,13 +25,16 @@ return {
           vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
 
           -- Buffer local mappings
-          local opts = { buffer = ev.buf }
-          vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-          vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-          vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-          vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-          vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
+          -- local opts = { buffer = ev.buf }
+          wk.add({
+            { '<leader>l', group = 'lsp' },
+            { '<leader>ld', vim.lsp.buf.definition, buffer = ev.buf, desc = 'Jumps to the definition of the symbol under the cursor.' },
+            { '<leader>lD', vim.lsp.buf.declation, buffer = ev.buf, desc = 'Jumps to the declaration of the symbol under the cursor.' },
+            { '<leader>lk', vim.lsp.buf.hover, buffer = ev.buf, desc = 'Displays info about the symbol under the cursor' },
+            { '<leader>ls', vim.lsp.buf.signature_help, buffer = ev.buf, desc = 'Displays signature info about the symbol under the cursor' },
+            { '<leader>li', vim.lsp.buf.implementation, buffer = ev.buf, desc = 'Lists all implementations of the symbol under the cursor.' },
+            { '<leader>lc', vim.lsp.buf.code_action, buffer = ev.buf, desc = 'Selects a code action available at the current cursor position.' },
+          })
         end,
       })
     end,
