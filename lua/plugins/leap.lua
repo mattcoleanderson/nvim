0247f667407702477523c5715ad246393b695953
Matthew Anderson <matthewanderson@FO-MAnd-MBPM1P.local>
2024-08-30 10:54:32 -0500

    Add leap to nvim

---
+++ lua/plugins/leap.lua
@@ -0,0 +1,16 @@
+local M = { 'ggandor/leap.nvim' }
+
+M.lazy = false
+
+M.config = function()
+  -- Set up Keymap
+  local wk = require('which-key')
+  wk.add({
+    { 's', '<Plug>(leap)' },
+    { 'S', '<Plug>(leap-from-window)' },
+    { 's', '<Plug>(leap-forward)', mode = { 'x', 'o' } },
+    { 'S', '<Plug>(leap-forward)', mode = { 'x', 'o' } },
+  })
+end
+
+return M
