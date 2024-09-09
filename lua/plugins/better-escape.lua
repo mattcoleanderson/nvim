d3a44eff55da5f00eb0db25ca2f06525a3679b5b
Matthew Anderson <mattcoleanderson@FO-MAnd-MBPM2P.local>
2024-09-09 17:03:57 -0500

    Remove jk from keymaps for better-escape.lua
    caused issues with lazygit.nvim

--- lua/plugins/better-escape.lua
+++ lua/plugins/better-escape.lua
@@ -2,6 +2,35 @@ return {
   {
     'max397574/better-escape.nvim',
     main = 'better_escape',
-    config = true,
+    opts = {
+      default_mappings = false,
+      mappings = {
+        i = { -- insert mode
+          j = { -- first key
+            j = '<Esc>', -- second key
+          },
+        },
+        c = { --command mode
+          j = {
+            j = '<Esc>',
+          },
+        },
+        t = {
+          j = {
+            j = '<C-\\><C-n>',
+          },
+        },
+        v = {
+          j = {
+            j = '<Esc>',
+          },
+        },
+        s = {
+          j = {
+            j = '<Esc>',
+          },
+        },
+      },
+    },
   },
 }
