248ba6ba33682cfa0bfd587be004402932bc1def
Matthew Anderson <mattcoleanderson@FO-MAnd-MBPM2P.local>
2024-09-11 11:40:19 -0500

    refactor none-ls to use opts instead of config

--- lua/plugins/lsp/none-ls.lua
+++ lua/plugins/lsp/none-ls.lua
@@ -2,18 +2,21 @@
 -- An API to expose Formatters, Linters, and Parsers to the LSP API
 -- These tools are typically command line.
 -- with none-ls they can be used with editor commands and events
-return {
+local M = {
   'nvimtools/none-ls.nvim',
   dependency = { 'nvim-lua/plenary.nvim' },
   enabled = vim.g.plugins.none_ls,
-  config = function()
-    local null_ls = require('null-ls') -- none-ls is still internally called null-ls
-
-    null_ls.setup({
-      -- Add Formatters, Linters, and Parsers you'd wish to be exposed to LSP API
-      sources = {
-        null_ls.builtins.formatting.stylua,
-      },
-    })
-  end,
+  main = 'null-ls',
 }
+
+M.opts = function()
+  local none_ls = require(M.main)
+
+  return {
+    sources = {
+      none_ls.builtins.formatting.stylua,
+    },
+  }
+end
+
+return M
