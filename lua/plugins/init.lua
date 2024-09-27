67b52e5b9960aec456ca3282adc0fe35b324f269
Matthew Anderson <mattcoleanderson@FO-MAnd-MBPM2P.local>
2024-09-27 00:30:19 -0500

    move html plugins under own dir

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,6 +1,7 @@
 return {
   { import = 'plugins.lsp' },
   { import = 'plugins.lang' },
+  { import = 'plugins.lang.html' },
   { import = 'plugins.explorers' },
   { import = 'plugins.snippets' },
 }
