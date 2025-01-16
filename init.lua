61765b0eed9303bba24b572a8fd6b7a251b071f7
Matthew Anderson <matt.anderson@Matts-MacBook-Pro.local>
2025-01-15 23:25:57 -0600

    set colorscheme to catppuccin-mocha

--- init.lua
+++ init.lua
@@ -25,3 +25,5 @@ require('toggle-plugins')
 require('lazy').setup('plugins') -- top-level files in lua/plugins will be auto-required
 require('keymap') -- ran last so which-key can be used, might create a second file for which-key mappings
 require('filetype')
+
+vim.cmd("colorscheme catppuccin-mocha")
