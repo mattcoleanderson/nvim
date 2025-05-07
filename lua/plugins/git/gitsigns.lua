0f30c3b2602c3de2ec4b750124659b35321354db
Matt Cole Anderson <git@mattcoleanderson.com>
2025-05-07 11:09:22 -0500

    add keymaps to jump to git changes

--- lua/plugins/git/gitsigns.lua
+++ lua/plugins/git/gitsigns.lua
@@ -3,6 +3,19 @@ local M = {
   enabled = vim.g.plugins.gitsigns,
 }
 
-M.config = true
+-- M.config = true
+M.config = function(_, opts)
+  local plugin = require('gitsigns')
+
+  -- Set up Keymaps
+  local wk = require('which-key')
+  wk.add({
+    { '<leader>v', group = 'vcs' },
+    { '<leader>vn', '<cmd>Gitsigns next_hunk<CR>', desc = 'Go to Next Hunk' },
+    { '<leader>vp', '<cmd>Gitsigns prev_hunk<CR>', desc = 'Go to Previous Hunk' },
+  })
+
+  plugin.setup(opts)
+end
 
 return M
