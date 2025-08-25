db6c58712f65e9f588e9180832c06e48b5fe4ceb
Matt Cole Anderson <git@mattcoleanderson.com>
2025-08-25 11:43:14 -0500

    add hotkeys for auto-sessions and add session name to lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,9 +3,16 @@ return {
     'nvim-lualine/lualine.nvim',
     enabled = vim.g.plugins.lualine,
     dependencies = { 'nvim-tree/nvim-web-devicons' },
+
+    ---@module 'lualine'
     opts = {
       options = {
         theme = 'auto',
+      },
+      sections = {
+        lualine_c = {
+          function() return require('auto-session.lib').current_session_name(true) end,
+        },
       },
     },
   },
--- lua/plugins/sessions/auto-session.lua
+++ lua/plugins/sessions/auto-session.lua
@@ -2,6 +2,15 @@ local M = {
   'rmagatti/auto-session',
   enabled = vim.g.plugins.auto_session,
   lazy = false,
+  keys = function()
+    local wk = require('which-key')
+    wk.add({ '<leader>S', group = 'sessions' })
+    return {
+      { "<leader>Sf", "<cmd>SessionSearch<CR>",         desc = "Session search" },
+      { "<leader>Ss", "<cmd>SessionSave<CR>",           desc = "Save session" },
+      { "<leader>Sa", "<cmd>SessionToggleAutoSave<CR>", desc = "Toggle autosave" },
+    }
+  end,
 
   ---@module 'auto-session'
   ---@type AutoSession.Config
