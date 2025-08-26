26d836ecc26f729f86ea4aef9ad5572ccf14308e
mattcoleanderson <matt@mattcoleanderson.com>
2025-08-26 02:12:10 -0500

    update winbar and statusline with lualine

--- lua/options.lua
+++ lua/options.lua
@@ -5,30 +5,6 @@ There are many ways to set vim options including:
   vim.o.wrap = false
 ]]
 
--- TODO: create a better winbar suited to my needs, also update the statu line (lualine plugin) as well
-local function status_line()
-  local mode = '%-5{%v:lua.string.upper(v:lua.vim.fn.mode())%}'
-  local file_name = '%-.16t'
-  local buf_nr = '[%n]'
-  local modified = ' %-m'
-  local file_type = ' %y'
-  local right_align = '%='
-  local line_no = '%10([%l/%L%)]'
-  local pct_thru_file = '%5p%%'
-
-  return string.format(
-    '%s%s%s%s%s%s%s%s',
-    mode,
-    file_name,
-    buf_nr,
-    modified,
-    file_type,
-    right_align,
-    line_no,
-    pct_thru_file
-  )
-end
-
 -- Set all vim options in this table
 local options = {
   number = true, -- Show absolute line numbers
@@ -42,7 +18,6 @@ local options = {
   signcolumn = 'auto:3', -- Allows the signcolumn (gutter) to display up to X signs per line
   laststatus = 3,
   -- winborder = 'single', -- adds border to floating windows
-  winbar = status_line(),
 }
 
 -- The above table will be iterated to set options individually
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,3 +1,5 @@
+local cyberdream = require('lualine.themes.cyberdream')
+
 return {
   {
     'nvim-lualine/lualine.nvim',
@@ -7,12 +9,40 @@ return {
     ---@module 'lualine'
     opts = {
       options = {
-        theme = 'auto',
+        -- theme = require('lualine.themes.dracula'),
+        theme = 'cyberdream',
+        refresh = {
+          statusline = 100,
+          tabline = 100,
+          winbar = 10,
+        },
       },
       sections = {
-        lualine_c = {
-          function() return require('auto-session.lib').current_session_name(true) end,
-        },
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_c = { { 'lsp_status', ignore_lsp = { 'null-ls' } } },
+        -- lualine_c = {
+        --   function() return require('auto-session.lib').current_session_name(true) end,
+        -- },
+        lualine_x = { 'encoding', 'fileformat', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' },
+      },
+      winbar = {
+        lualine_a = { { 'filetype', icon_only = true } },
+        lualine_b = { { 'filename', path = 1 } },
+        lualine_c = { 'diff' },
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = { 'aerial' },
+      },
+      inactive_winbar = {
+        lualine_a = { 'filename' },
+        lualine_b = { 'diff' },
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {},
       },
     },
   },
