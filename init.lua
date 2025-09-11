9b42f15f7c515adb3bae4bf36950539e1d6751bd
mattcoleanderson <matt@mattcoleanderson.com>
2025-09-11 00:20:02 -0500

    add new command to build and run currently open buffer if C program

--- init.lua
+++ init.lua
@@ -20,9 +20,10 @@ vim.g.maplocalleader = ',' -- remap localleaer to backspace
 
 -- Run all other lua files
 require('options')
-require('commands')
 require('toggle-plugins')
 require('lazy').setup('plugins') -- top-level files in lua/plugins will be auto-required
+require('autocmds')
+require('commands')
 require('keymap') -- ran last so which-key can be used, might create a second file for which-key mappings
 require('filetype')
 
