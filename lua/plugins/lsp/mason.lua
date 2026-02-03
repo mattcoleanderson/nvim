d06ca7d93ffa5ddf976cafeb172d723c4e14a502
Matt Cole Anderson <github@mattcoleanderson.com>
2026-02-03 01:51:03 -0600

    mason.nvim migrated to a github organization, mason-org

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -1,12 +1,12 @@
 -- Mason is a package manager for LSPs, Linters, Parsers, and Formatters
 return {
   {
-    'williamboman/mason.nvim', -- Mason Package manager
+    'mason-org/mason.nvim', -- Mason Package manager
     enabled = vim.g.plugins.mason,
     config = true,
   },
   {
-    'williamboman/mason-lspconfig.nvim', -- Helper function to explicitly state LSPs to install.
+    'mason-org/mason-lspconfig.nvim', -- Helper function to explicitly state LSPs to install.
     enabled = vim.g.plugins.mason_lspconfig,
     opts = {
       automatic_enable = false, -- prevents duplicate lsp attachment if setup is called elsewhere
