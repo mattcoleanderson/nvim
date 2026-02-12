5b37e5d100c63e2a0dd8c4799380f5c9acba046e
Matt Cole Anderson <github@mattcoleanderson.com>
2026-02-12 10:25:13 -0600

    copy file path command added to neotree

--- lua/plugins/explorers/neotree.lua
+++ lua/plugins/explorers/neotree.lua
@@ -117,7 +117,41 @@ return {
       -- A list of functions, each representing a global custom command
       -- that will be available in all sources (if not overridden in `opts[source_name].commands`)
       -- see `:h neo-tree-custom-commands-global`
-      commands = {},
+      commands = {
+        -- Copy File path
+        --    found this mapping here: https://github.com/nvim-neo-tree/neo-tree.nvim/discussions/370#discussioncomment-4144005
+        copy_fle_path = function(state)
+          -- NeoTree is based on [NuiTree](https://github.com/MunifTanjim/nui.nvim/tree/main/lua/nui/tree)
+          -- The node is based on [NuiNode](https://github.com/MunifTanjim/nui.nvim/tree/main/lua/nui/tree#nuitreenode)
+          local node = state.tree:get_node()
+          local filepath = node:get_id()
+          local filename = node.name
+          local modify = vim.fn.fnamemodify
+
+          local results = {
+            filepath,
+            modify(filepath, ':.'),
+            modify(filepath, ':~'),
+            filename,
+            modify(filename, ':r'),
+            modify(filename, ':e'),
+          }
+
+          vim.ui.select({
+            '1. Absolute path: ' .. results[1],
+            '2. Path relative to CWD: ' .. results[2],
+            '3. Path relative to HOME: ' .. results[3],
+            '4. Filename: ' .. results[4],
+            '5. Filename without extension: ' .. results[5],
+            '6. Extension of the filename: ' .. results[6],
+          }, { prompt = 'Choose to copy to clipboard:' }, function(choice)
+            local i = tonumber(choice:sub(1, 1))
+            local result = results[i]
+            vim.fn.setreg('"', result)
+            vim.notify('Copied: ' .. result)
+          end)
+        end,
+      },
       window = {
         position = 'left',
         width = 40,
@@ -175,6 +209,8 @@ return {
           ['<'] = 'prev_source',
           ['>'] = 'next_source',
           ['i'] = 'show_file_details',
+
+          ['Y'] = 'copy_fle_path',
         },
       },
       nesting_rules = {},
